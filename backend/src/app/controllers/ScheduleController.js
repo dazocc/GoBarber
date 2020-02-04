aec454d44949ca499341df5ff6fb5e44456ca4bf
dazocc <dazocc@gmail.com>
2020-02-03 20:43:14 -0400

    first commit

---
+++ backend/src/app/controllers/ScheduleController.js
@@ -0,0 +1,53 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/User';
+// import File from '../models/File';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const { page = 1, date } = req.query;
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+      // limit: 20,
+      offset: (page - 1) * 20,
+      // attributes: ['id', 'date'],
+      // include: [
+      //   {
+      //     model: User,
+      //     as: 'provider',
+      //     attributes: ['id', 'name'],
+      //     include: [
+      //       {
+      //         model: File,
+      //         as: 'avatar',
+      //         attributes: ['id', 'path', 'url'],
+      //       },
+      //     ],
+      //   },
+      // ],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://a0d8aa2340f74ef383cf5fb8fab10fb6@sentry.io/2184239',
+};
